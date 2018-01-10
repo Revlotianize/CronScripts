2b036315e8cd19b155b4674fb50b3a5ef5255140
Revlotianize <kartik.madnani123@gmail.com>
2018-01-10 11:50:09 +0530

    descriptivemessage

--- conscript.php
+++ conscript.php
@@ -1,12 +1,14 @@
 <?php  
 	// passthru("git init");
 	// passthru("git remote add $project_name $project_git_url");
-	passthru("git init");
-	passthru("git add .");
-	passthru("git commit -m 'descriptivemessage' ");
-	passthru("status");
-	passthru("git push origin master ");
+	// passthru("git init");
+	// passthru("git add .");
+	// passthru("git commit -m 'descriptivemessage' ");
+	// passthru("status");
+	// passthru("git push origin master ");
 	// passthru("git config --global user.name Revlotianize");
 	passthru("git status");
+	 passthru("git add .");
+	passthru("git commit -m 'descriptivemessage' ");
 
 	?>
